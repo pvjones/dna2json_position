9d16ff190e24b6ca7e39d8814b6a469799366955
Paul Jones <paulvjones@gmail.com>
2017-02-14 19:05:07 -0700

    patched Ancestry bug, all parsers now return position as well

--- examples/toJSON.js
+++ examples/toJSON.js
@@ -1,8 +1,8 @@
 var dna = require('../');
 var fs = require('fs');
 var path = require('path');
-
-var txt = fs.readFileSync(path.join(__dirname, 'dna.txt'));
+console.log(__dirname)
+var txt = fs.readFileSync(path.join(__dirname, 'dna.txt'), 'utf8');
 
 dna.parse(txt, function(err, snps){
   fs.writeFileSync(path.join(__dirname, 'dna.json'), JSON.stringify(snps));
